4d1eb14e9cd6af90d155d9e1f10fe385f5306432
teerakarn1234 <39583891+teerakarn1234@users.noreply.github.com>
2018-05-24 15:38:01 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -12,6 +12,8 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
+	echo "OK ****00000 ";
+	exit();
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
